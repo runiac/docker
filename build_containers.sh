3c030f48e0273edd125ca76aaa899784aa389ceb
mg <grose.matthewf@gmail.com>
2021-05-31 18:19:05 -0500

    Update build_containers.sh
    always keep latest [release] up to date but also provide ability for clients to flag a specific version.  changed main builds to be main versioned

--- build_containers.sh
+++ build_containers.sh
@@ -1,23 +1,32 @@
 #!/bin/bash
+push_containers () {
+    local image_prefix="runiac/deploy:$1-"
+
+    echo $image_prefix
+
+    docker buildx build --build-arg RUNIAC_REF=$REF --platform linux/arm64,linux/amd64 -f "package/alpine/Dockerfile" -t "${image_prefix}alpine" --push . || exit 1
+    docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-aws/Dockerfile" -t "${image_prefix}alpine-aws" --push . || exit 1
+    docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-azure/Dockerfile" -t "${image_prefix}alpine-azure" --push . &
+    docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-full/Dockerfile" -t "${image_prefix}alpine-full" --push . &
+    docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-gcp/Dockerfile" -t "${image_prefix}alpine-gcp" --push . &
+
+    wait
+}
 
 # if version not set, set to local default
 if [[ "$REF" == "refs/heads/main" ]]; then
-  VERSION="latest"
+  VERSION="main"
 elif  [[ "$REF" =~ refs/tags/* ]]; then  
   VERSION=$(echo ${REF#refs/tags/})
+  latest="true"
 else
   VERSION=$(whoami)
   REF='refs/heads/main'
 fi
 
-image_prefix="runiac/deploy:$VERSION-"
+push_containers $VERSION
 
-echo $image_prefix
-
-docker buildx build --build-arg RUNIAC_REF=$REF --platform linux/arm64,linux/amd64 -f "package/alpine/Dockerfile" -t "${image_prefix}alpine" --push . || exit 1
-docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-aws/Dockerfile" -t "${image_prefix}alpine-aws" --push . || exit 1
-docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-azure/Dockerfile" -t "${image_prefix}alpine-azure" --push . &
-docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-full/Dockerfile" -t "${image_prefix}alpine-full" --push . &
-docker buildx build --platform linux/arm64,linux/amd64 -f "package/alpine-gcp/Dockerfile" -t "${image_prefix}alpine-gcp" --push . &
-
-wait
+if [[ $latest == "true" ]]
+then
+  push_containers "latest"
+fi
